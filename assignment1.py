fecbb8c44ee169a00e9010c8e1925cad21a9757a
ZhuKunBo <kunbo.zhu@my.jcu.edu.au>
2018-12-08 10:15:48 +0800

    add the docstring and pseudocode

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,48 @@
+"""
+The program is for loading,adding and completing songs
+Name:ZhuKunBo
+Date:December 7th,2018
+Link to my project on GitHub:https://github.com/jc490479/CP1404-SP532018-STARTER-A1
+
+Function load_songs():
+import csv
+csv_reader=csv.reader(open("songs.csv"))
+for row in csv_reader
+list.append(row)
+for index,element in enumerate(list)
+if element[3] is y, then
+elemnet[3] = "*"
+Otherwise
+element[3] = " "
+Display " L - list songs
+          A - add new song
+          C - complete a song
+          Q - quit"
+get a choice from user
+if choice is L, then
+for i, item in enumerate(list):
+Display""{:2}".format(i), "{:2}{:<30s}{:<20s}{:>20s}".format
+                    (item[3], item[0], item[1], int(item[2]))"
+
+Function complete_song():
+while True:
+  try:
+     complete = int(input("Enter the number of a song to mark as learned:"))
+  except ValueError:
+     Display "invalid input,enter a valid number"
+  else:
+      break
+while complete < 0:
+    Display"number must >= 0"
+    omplete = int(input("Enter the number of a song to mark as learned:"))
+
+if list[complete][3] == " ", then
+    Display""you have already learned {}".format(list[complete][0])"
+else:
+    list[complete][3] = " "
+    Display""{} by {} learned".format(list[complete][0], list[complete][1]))"
+
+"""
 
 
 import csv
@@ -23,8 +68,8 @@ def main():
     while choice != "Q":
         if choice == "L":
             for i, item in enumerate(list):
-                print("{:2}".format(i), "{:2}{:<30s}{:<20s}{:>20s}".format
-                (item[3], item[0], item[1], item[2]))
+                print("{:2}".format(i), "{:2}{:<30s}{:<20s}{:>20d}".format
+                    (item[3], item[0], item[1], int(item[2])))
             menu = show_menu()
             choice = input("Enter a option:").upper()
         elif choice == "A":
@@ -35,8 +80,17 @@ def main():
             choice = complete_song()
             menu = show_menu()
             choice = input("Enter a option:").upper()
+        else:
+            print("Invalid choice,please input a valid choice")
+            menu = show_menu()
+            choice = input("Enter a option:").upper()
+
+    if choice == "Q":
+        print("songs save to songs.csv,have a nice day:")
+
 
 def show_menu():
+    """Print a menu"""
     print("Menu:")
     print("L","-","List songs")
     print("A","-", "Add new song")
@@ -44,25 +98,50 @@ def show_menu():
     print("Q","-","Quit")
 
 def add_song():
+    """Add the song(title,writer,year) in the menu"""
     adds = []
     title = input("Enter the song's title,input can not be blank:")
+
+    while title == "":
+        title = input("Input can not be blank,please input title a valid format:")
     adds.append(title)
-    writer = input("Enter the song's writer,input can not be blank:")
+    writer = input("Enter the song's writer：")
+    while writer =="":
+     writer = input("Input can not be blank,please input writer a valid format:")
     adds.append(writer)
-    year = input("Enter the year,number must >= 0:")
+    while True:
+        try:
+            year = int(input("enter the year:"))
+        except ValueError:
+            print("Invalid input,enter a valid number")
+        else:
+            break
+
+    while year <= 0:
+        print("number must >= 0")
+        year = input("Enter the year")
     adds.append(year)
     require = "*"
     adds.append(require)
     list.append(adds)
 
 def complete_song():
-    complete = int(input("Enter the number of a song to mark as learned:"))
-    if list[complete][3] == "*":
+    """Complete the song's learning"""
+    while True:
+        try:
+            complete = int(input("Enter the number of a song to mark as learned:"))
+        except ValueError:
+            print("Invalid input,enter a valid number")
+        else:
+            break
+    while complete < 0:
+        print("number must >= 0")
+        complete = int(input("Enter the number of a song to mark as learned:"))
+
+    if list[complete][3] == " ":
+        print("you have already learned {}".format(list[complete][0]))
+    else:
         list[complete][3] = " "
-        print("the song learned successfully")
-    elif list[complete][3] == " ":
-        print("songs already learned")
-
-
+        print("{} by {} learned".format(list[complete][0], list[complete][1]))
 
 main()
